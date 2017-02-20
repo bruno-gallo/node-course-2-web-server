1923347ece5c266c0952942a8fd1ed04c99e73ca
Tangocode <tangocode@MacBook-Air-de-Tangocode.local>
2017-02-20 01:54:43 -0300

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+// variable de entorno seteada por heroku
+const port = process.env.port || 3000;
 var app = express();
 
 //define directorio de las partials (templates)
@@ -56,6 +58,6 @@ app.get('/bad', (request, response) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log("Server is up on port 3000");
+app.listen(port, () => {
+    console.log(`Server is up on ${port}`);
 });
